246b7b70eedd241f277b6963ff896343eea2e795
mharsh911 <mharsh911@gmail.com>
2025-06-28 21:08:13 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+// server.js
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+server.use((_req, _res, next) => {
+  setTimeout(next, 2000); // 800ms delay
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
